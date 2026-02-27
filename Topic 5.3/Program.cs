d542d3c95493a1328e723d4d6c1758e3be5947f6
362Sadie-F <princessmae.fleming@gmail.com>
2026-02-27 12:28:48 -0500

    task 1

--- Topic 5.3/Program.cs
+++ Topic 5.3/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double money;
+            string work;
+
+            //task 1
+            Console.WriteLine("You want to go to the movies? Are you working?");
+            work = Console.ReadLine();
+            if (work == "no" || work == "yes")
+            {
+                Console.WriteLine("Okay, and how much do you have?");
+            }
+            money = Convert.ToDouble(Console.ReadLine());
+            if (money >= 20 && work == "no")
+            {
+                Console.WriteLine("You have " + money.ToString("C") + " and aren't going to work, so you can go!");
+            }
+            if (money < 20 && work == "no")
+            {
+                Console.WriteLine(money.ToString("C") + " Is not enough to go.");
+            }
+            if (money >= 20 && work == "yes")
+            {
+                Console.WriteLine("You have work and can't go.");
+            }
+            if (money < 20 && work == "yes")
+            {
+                Console.WriteLine("You have work and can't go.");
+            }
+            Console.WriteLine("Press ENTER to continue");
+            Console.ReadLine();
+            Console.Clear();
+
+            //task 2
+
         }
     }
 }
